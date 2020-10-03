932b9c185b0ce5ae698704d6c5592d549743ce1a
akyc <23010109+akyc@users.noreply.github.com>
2020-10-03 21:04:19 +0300

    Личный проект: маршрут по тайным тропам

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import MainPage from "../main-page/main-page";
-
+import LoginPage from "../login-page/login-page";
+import FavoritesPage from "../favorites-page/favorites-page";
+import OfferPage from "../offer-page/offer-page";
 
 const App = (props) => {
   const {placesFound} = props;
 
   return (
-    <MainPage placesFound={placesFound} />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <MainPage placesFound={placesFound} />
+        </Route>
+        <Route path="/login" exact>
+          <LoginPage />
+        </Route>
+        <Route path="/favorites" exact>
+          <FavoritesPage />
+        </Route>
+        <Route path="/offer/:id" exact>
+          <OfferPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
